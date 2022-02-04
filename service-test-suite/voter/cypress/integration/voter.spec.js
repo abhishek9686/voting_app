c35b15b482b950d06dcb45273ec51eb3d5ba5c24
Abhishek K <abhi281342@gmail.com>
2022-02-04 11:54:55 +0530

    change voter url

--- service-test-suite/voter/cypress/integration/voter.spec.js
+++ service-test-suite/voter/cypress/integration/voter.spec.js
@@ -6,45 +6,54 @@
 // https://on.cypress.io/writing-first-test
 describe('My First Test', () => {
   it('Visits voter webpage', () => {
-    cy.visit('http://roost-controlplane:30030/voter')
+    cy.visit('http://current-cluster-roost.io/voter');
 
-  cy.intercept(
-  {
-    method: 'POST', // Route all POST requests
-    url: '*ballot*', // that have a URL that matches '/users/*'
-  }
-).as('postresult')
+    cy.intercept({
+      method: 'POST', // Route all POST requests
+      url: '*ballot*', // that have a URL that matches '/users/*'
+    }).as('postresult');
 
-cy.contains("Docker").click()
+    cy.contains('Docker').click();
 
-cy.wait('@postresult',{ responseTimeout: 35000 }).then((interception) => {
+    cy.wait('@postresult', { responseTimeout: 35000 }).then((interception) => {
+      assert.isNotNull(
+        interception.response.body,
+        '{code: 201, message: "Vote saved sucessfully"}'
+      );
+    });
 
-  assert.isNotNull(interception.response.body, '{code: 201, message: "Vote saved sucessfully"}')
-})
+    cy.reload();
+    cy.contains('MiniKube').click();
 
-cy.reload()
-cy.contains("MiniKube").click()
+    cy.wait('@postresult', { responseTimeout: 35000 }).then((interception) => {
+      assert.isNotNull(
+        interception.response.body,
+        '{code: 201, message: "Vote saved sucessfully"}'
+      );
+    });
+    cy.reload();
+    cy.contains('Roost').click();
 
-cy.wait('@postresult',{ responseTimeout: 35000 }).then((interception) => {
-  assert.isNotNull(interception.response.body, '{code: 201, message: "Vote saved sucessfully"}')
-})
-cy.reload()
-cy.contains("Roost").click()
+    cy.wait('@postresult', { responseTimeout: 35000 }).then((interception) => {
+      assert.isNotNull(
+        interception.response.body,
+        '{code: 201, message: "Vote saved sucessfully"}'
+      );
+    });
+    cy.reload();
+    cy.contains('K3D').click();
 
-cy.wait('@postresult',{ responseTimeout: 35000 }).then((interception) => {
-  assert.isNotNull(interception.response.body, '{code: 201, message: "Vote saved sucessfully"}')
-})
-cy.reload()
-cy.contains("K3D").click()
+    cy.wait('@postresult', { responseTimeout: 35000 }).then((interception) => {
+      assert.isNotNull(
+        interception.response.body,
+        '{code: 201, message: "Vote saved sucessfully"}'
+      );
+    });
 
-cy.wait('@postresult',{ responseTimeout: 35000 }).then((interception) => {
-  assert.isNotNull(interception.response.body, '{code: 201, message: "Vote saved sucessfully"}')
-})
-
-cy.visit('http://roost-controlplane:30030/voter/result')
-cy.contains('Roost')
-cy.contains('Docker')
-cy.contains('MiniKube')
-cy.contains('K3D')
-  })
-})
+    cy.visit('http://current-cluster-roost.io/voter/result');
+    cy.contains('Roost');
+    cy.contains('Docker');
+    cy.contains('MiniKube');
+    cy.contains('K3D');
+  });
+});
